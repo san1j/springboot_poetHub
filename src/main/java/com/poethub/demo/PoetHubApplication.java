e97a3f42d0a566670e04642d7b44e8e3bfaa069e
san1j <sanjanasreenath96@gmail.com>
2018-05-21 19:26:11 -0400

    initial commit

---
+++ src/main/java/com/poethub/demo/PoetHubApplication.java
@@ -0,0 +1,12 @@
+package com.poethub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoetHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoetHubApplication.class, args);
+	}
+}
