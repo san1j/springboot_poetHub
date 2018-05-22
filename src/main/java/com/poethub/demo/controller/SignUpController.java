737acf7a4f5140a730f91cdd2b88591e5c898da5
san1j <sanjanasreenath96@gmail.com>
2018-05-22 17:24:09 -0400

    validate form

--- src/main/java/com/poethub/demo/controller/SignUpController.java
+++ src/main/java/com/poethub/demo/controller/SignUpController.java
@@ -1,5 +1,6 @@
 package com.poethub.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/SignUp")
 public class SignUpController {
+	@Autowired
+	private userRepo userRepo;
+	
 	@RequestMapping(method = RequestMethod.GET)
  	public String home() {
 		return "signup";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String signup() {
+		return 
 	}
 }
