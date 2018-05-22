12cd62d8d87d468e0300d765a0074bc92537efd2
san1j <sanjanasreenath96@gmail.com>
2018-05-22 17:26:18 -0400

    validate forms

--- src/main/java/com/poethub/demo/controller/LoginController.java
+++ src/main/java/com/poethub/demo/controller/LoginController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class LoginController {
 	@RequestMapping(method = RequestMethod.GET)
  	public String home() {
-		return "login" ;
+		return "login";
 	}
 }
--- src/main/java/com/poethub/demo/controller/SignUpController.java
+++ src/main/java/com/poethub/demo/controller/SignUpController.java
@@ -1,23 +1,34 @@
 package com.poethub.demo.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+ 
+import com.poethub.demo.model.userModel;
 
 @Controller
-@RequestMapping("/SignUp")
 public class SignUpController {
-	@Autowired
-	private userRepo userRepo;
+//	@Autowired
+//	private userRepo userRepo;
 	
-	@RequestMapping(method = RequestMethod.GET)
- 	public String home() {
+	@GetMapping("/SignUp")
+ 	public String home(Model model) {
+		model.addAttribute("userModel", new userModel());
 		return "signup";
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
-	public String signup() {
-		return 
+	@PostMapping("/SignUp")
+	public String signup(@Valid userModel userModel, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+            return "signup";
+        }
+	    return "tempObject";
 	}
+		
 }
