2fd383692c6f7a925a2932706d7e05040f656123
san1j <sanjanasreenath96@gmail.com>
2018-05-25 19:31:27 -0400

    refactor code and various style fixes

--- src/main/java/com/poethub/demo/controller/HomeController.java
+++ src/main/java/com/poethub/demo/controller/HomeController.java
@@ -1,14 +1,26 @@
 package com.poethub.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.poethub.demo.model.PoemModel;
+import com.poethub.demo.service.PoemService;
  
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	@Autowired
+	private PoemService poemService;
+	
 	@RequestMapping(method = RequestMethod.GET)
- 	public String home() {
+ 	public String home(Model model) {
+		List<PoemModel> poems = poemService.findAllByOrderByPoemIdDesc();
+		model.addAttribute("allpoems", poems);
 		return "home";
 	}
 }
