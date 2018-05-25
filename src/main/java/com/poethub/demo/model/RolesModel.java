e015fd7a93cb7d5fb04fc2c249edbfe3f3ec02eb
san1j <sanjanasreenath96@gmail.com>
2018-05-25 19:33:45 -0400

    refactor

--- src/main/java/com/poethub/demo/model/RolesModel.java
+++ src/main/java/com/poethub/demo/model/RolesModel.java
@@ -1,46 +1,48 @@
-package com.poethub.demo.model;
-
-import java.util.Collection;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "ROLES")
-public class RolesModel {
-	  @Id
-	  @GeneratedValue(strategy = GenerationType.AUTO)
-	  @Column(name ="roleId")
-      private int roleId;
-	  @Column(name = "role", nullable = false) 
-      private String role;
-	  @ManyToMany(mappedBy = "roles", cascade = CascadeType.ALL)
-      private Collection<UserModel> users;
-		
-	  public int getRoleId() {
-			return roleId;
-		}
-	  public void setRoleId(int roleId) {
-			this.roleId = roleId;
-		}
-      public String getRole() {
-			return role;
-		}
-	  public void setRole(String role) {
-			this.role = role;
-		}
-      public Collection<UserModel> getUsers() {
-			return users;
-		}
-	  public void setUsers(Collection<UserModel> users) {
-			this.users = users;
-		}
-	  
-	  
-}
+//package com.poethub.demo.model;
+//
+//import java.util.Collection;
+//
+//import javax.persistence.CascadeType;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.ManyToMany;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name="ROLES")
+//public class RolesModel {
+//	  @Id
+//	  @GeneratedValue(strategy = GenerationType.AUTO)
+//	  @Column(name ="roleId")
+//      private int roleId;
+//	  
+//	  @Column(name = "role", nullable = false) 
+//      private String role;
+//	  
+//	  @ManyToMany(mappedBy = "roles", cascade = CascadeType.ALL)
+//      private Collection<UserModel> users;
+//		
+//	  public int getRoleId() {
+//			return roleId;
+//		}
+//	  public void setRoleId(int roleId) {
+//			this.roleId = roleId;
+//		}
+//      public String getRole() {
+//			return role;
+//		}
+//	  public void setRole(String role) {
+//			this.role = role;
+//		}
+//      public Collection<UserModel> getUsers() {
+//			return users;
+//		}
+//	  public void setUsers(Collection<UserModel> users) {
+//			this.users = users;
+//		}
+//	  
+//	  
+//}
--- src/main/java/com/poethub/demo/service/PoemServiceImpl.java
+++ src/main/java/com/poethub/demo/service/PoemServiceImpl.java
@@ -1,9 +1,12 @@
 package com.poethub.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.poethub.demo.model.PoemModel;
+import com.poethub.demo.model.UserModel;
 import com.poethub.demo.repo.PoemRepo;
 
 @Service
@@ -13,8 +16,17 @@ public class PoemServiceImpl implements PoemService{
 	
 	@Override
 	public void savePoem(PoemModel poemModel) {
-		poemRepo.save(poemModel);
-		
+		poemRepo.saveAndFlush(poemModel);
+	}
+
+	@Override
+	public List<PoemModel> findAllByOrderByPoemIdDesc() {
+		return poemRepo.findAllByOrderByPoemIdDesc();
+	}
+
+	@Override
+	public List<PoemModel> findAllByUserModelOrderByPoemIdDesc(UserModel userModel) {
+		return poemRepo.findAllByUserModelOrderByPoemIdDesc(userModel);
 	}
 
 }
