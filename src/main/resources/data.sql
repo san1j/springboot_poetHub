f20f8cd0bb15ee32e248598d34b49364fa336496
san1j <sanjanasreenath96@gmail.com>
2018-05-28 16:30:29 -0400

    modified poems in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO USER (email,enabled,first_name,last_name,password,role,username,user_id) VALUES ('asdf@fs.sf',true,'firstName','lastName','$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'USER','username',1);
+INSERT INTO USER (email,enabled,first_name,last_name,password,role,username,user_id) VALUES ('asdf@fs.sf',true,'firstName','lastName','$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'USER','anonymous',1);
 
 INSERT INTO USER(email,enabled,first_name,last_name,password,role,username,user_id) VALUES ('ella@df.sd',true,'firstName','lastName','$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'USER','Ella Higginson',2);
 
@@ -58,23 +58,26 @@ On broken blinds and chimney-pots,
 And at the corner of the street
 A lonely cab-horse steams and stamps.','Preludes',3,3);
 
-INSERT INTO POEM(body,title,user_id,poem_id) VALUES ('Let us go then, you and I,
-When the evening is spread out against the sky
-Like a patient etherized upon a table;
-Let us go, through certain half-deserted streets,
-The muttering retreats
-Of restless nights in one-night cheap hotels
-And sawdust restaurants with oyster-shells:
-Streets that follow like a tedious argument
-Of insidious intent
-To lead you to an overwhelming question…
-Oh, do not ask, “What is it?”
-Let us go and make our visit.','The Love Song of J. Alfred Prufrock',3,2);
+INSERT INTO POEM(body,title,user_id,poem_id) VALUES ('The wrath of wind, it comes and goes,
+The fear of men, no longer blows,
+The hunger of fire, it does devour,
+But all who give in, it will sour,
+The strength of water, cannot be matched,
+By every evil scheme thats hatched,
+The life of earth, is unsurpassed,
+Unless you’ve seen, the future past.
+Do not be weathered, by elements strong,
+Their strength by no means, lasts that long.','Strength',1,2);
 
-INSERT INTO POEM(body,title,user_id,poem_id) VALUES ('Miss Helen Slingsby was my maiden aunt,	
-And lived in a small house near a fashionable square	
-Cared for by servants to the number of four.	
-Now when she died there was silence in heaven	
-And silence at her end of the street.','Aunt Helen',3,1);
-   
+INSERT INTO POEM (body,title,user_id,poem_id) VALUES ('The sun it shines, regardless,
+The grass it grows, oblivious,
+The water it sits, fathomless.
+The moon it reflects, lovingly,
+The tree it stands, determinedly,
+The sand it moves, impulsively.
+Time is a constant, unstoppable,
+Space is vast, incalculable,
+Life is a gift, remarkable.','Life',1,1);
+
+
     
