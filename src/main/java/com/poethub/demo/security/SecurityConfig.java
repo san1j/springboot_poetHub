70652a019aa001937621ea5cbc4a38e08daf2cd1
san1j <sanjanasreenath96@gmail.com>
2018-05-31 16:25:45 -0400

    remove custom oauth success handler

--- src/main/java/com/poethub/demo/security/SecurityConfig.java
+++ src/main/java/com/poethub/demo/security/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
- import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,21 +19,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private DataSource dataSource;
-	@Autowired
-	private CustomHandler customHandler;
 
 	@Value("${spring.user.username}")
 	private String username;
 	@Value("${spring.user.password}")
 	private String password;
-	
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
 				.antMatchers("/userPage/**", "/newPoem", "/editPoem/{id}", "/deletePoem/{id}").authenticated()
-				.anyRequest().permitAll().and().formLogin().loginPage("/login**").defaultSuccessUrl("/").permitAll().and()
-				.logout().logoutSuccessUrl("/").permitAll();
+				.anyRequest().permitAll().and().formLogin().loginPage("/login**").defaultSuccessUrl("/").permitAll()
+				.and().logout().logoutSuccessUrl("/").permitAll();
 
 	}
 
